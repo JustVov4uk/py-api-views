d7a581f24bf964025c8ea77cdf3c4a725e2ccf96
JustVov4uk <volodabudzan4@gmail.com>
2025-12-09 18:49:16 +0100

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-from cinema.models import Movie, Actor, Genre, CinemaHall
+from cinema.models import (
+    Movie,
+    Actor,
+    Genre,
+    CinemaHall
+)
 
 
 class ActorSerializer(serializers.Serializer):
